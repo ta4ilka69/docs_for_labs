1cdb4c28560f1550346e199ff1291edddf95c0df
ta4ilka69 <artem_balin_2004@mail.ru>
2022-11-08 11:12:27 +0300

    lab4 create

---
+++ lab4_convert.py
@@ -0,0 +1,58 @@
+f = open('./Monday.json', 'r', encoding='utf-8')
+F = f.readlines()
+f.close()
+s = ''
+for S in F:
+    s += S.strip()
+result = ''
+tab = -1
+i = 0
+name = True
+while i < len(s):
+    if s[i] == '{':
+        tab += 1
+        name = True
+        i += 1
+        if tab != 0:
+            result += '\n'
+    elif s[i] == '\"':
+        if name:
+            t = ''
+            i += 1
+            while s[i] != '\"':
+                t += s[i]
+                i += 1
+            i += 1
+            result += "  "*tab + t
+            name = False
+        else:
+            t = ''
+            i += 1
+            while s[i] != '\"':
+                t += s[i]
+                i += 1
+            i += 1
+            q = True
+            for x in t:
+                if x.isalpha():
+                    q = False
+                    break
+            if q:
+                result += "\'"+t+"\'"
+            else:
+                result += t
+            name = True
+    elif s[i] == ':':
+        result += ': '
+        i += 1
+    elif s[i] == ',':
+        result += '\n'
+        i += 1
+    elif s[i] == '}':
+        tab -= 1
+        i += 1
+    else:
+        i += 1
+f = open("./Monday.yaml", 'w', encoding='utf-8')
+f.write(result)
+f.close()

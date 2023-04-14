d7dba04ff9beba0ddec1a4113ce4868ecb004a4b
ta4ilka69 <artem_balin_2004@mail.ru>
2023-04-14 11:57:41 +0300

    queue writer +bzhd

--- queue_writer.py
+++ queue_writer.py
@@ -18,10 +18,10 @@ def submit_response(form_url,entry,verbose=False):
 urlbd = "https://docs.google.com/forms/d/17yKOnNs0rCKk8cZQWHeOAxuCF5nrjUmVqcNV1sFGenI/formResponse"
 urlprog = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfiYFjZkPDmIiXNgpKPNxvY2l00CanxoeT3tmxLUxKTAfPLYA/formResponse"
 urlopd = "https://docs.google.com/forms/d/1UiIDe0qF5VYkcDQVxwNHnmpVUXXFwIty8W_T1yUz6jk/formResponse"
-bd = ""
+bd = "entry.1543865908"
 prog = "entry.988308360"
 opd = ""
 global t
 t = False
 while not t:
-    print(submit_response(urlbd,bd))
+    print(submit_response(urlopd,opd))

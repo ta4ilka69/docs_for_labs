728201ab0c36a9bdfb2a30166c030c39f444fb97
ta4ilka69 <artem_balin_2004@mail.ru>
2023-06-01 16:40:52 +0300

    5,6 opd

--- ОПД/6/task.asm
+++ ОПД/6/task.asm
@@ -15,9 +15,87 @@ MIN_VALUE: WORD 0xFFEC  ;-20 минимальное значение по ОДЗ
 MAX_VALUE: WORD 0x16    ; 22 максимальное значение по ОДЗ
 ADDR: WORD $X           ;адрес ячейки X
 START:
-    DI
+    DI          ; запрет прерываний
     CLA
     LD #0x9     ; MR1 -> AC (1000|0001 -> 1001)
     OUT 3       ; разрешить прерывание на ВУ-1
     LD #0xB     ; MR3 -> AC (1000|0011 -> 1011)
-    OUT 7       ; разрешить прерывания на
+    OUT 7       ; разрешить прерывания на ВУ-3
+
+T1: WORD ?
+PROG: EI        ; разрешить прерывания
+    CLA
+    INC_LOOP:
+        LD $X
+        ST $T1
+        INC
+        CALL $CHECK_X
+        PUSH
+        LD $T1
+        PUSH
+        LD $ADDR
+        PUSH
+        CALL $CAS
+        SWAP
+        POP
+        SWAP
+        POP
+        SWAP
+        POP
+        JUMP $INC_LOOP
+
+
+CHECK_X:
+    CMP $MAX_VALUE
+    BPL SET_MIN
+    CMP $MIN_VALUE
+    BMI SET_MIN
+    RET
+    SET_MIN: 
+        LD $MIN_VALUE
+        RET
+
+T2: WORD ?
+INT1: 
+    HLT
+    PUSH
+    ST $T2
+    ADD $T2
+    ADD $T2         ;x3
+    ASL             ;x3 x2 = x6
+    SUB #8
+    OUT 2
+    POP
+    HLT
+    IRET
+
+Y3: WORD ?
+INT3: 
+    HLT
+    PUSH
+    CLA
+    IN 6
+    ST $Y3
+    CLA
+    SUB $Y3
+    SUB $Y3
+    SUB $Y3
+    ADD $X
+    ST $X
+    POP
+    HLT
+    IRET
+
+DEREF: WORD ?
+CAS: ;PS-0, RET - 1, ADR - 2, OLD_VALUE - 3, FUNC_VALUE - 4
+    PUSHF
+    DI
+    LD &2
+    ST $DEREF
+    LD &3
+    CMP (DEREF)
+    BNE OUT_CAS
+    LD &4
+    ST (DEREF)
+    OUT_CAS: POPF
+    RET

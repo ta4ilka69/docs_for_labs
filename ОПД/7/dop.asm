1aeeec28335ea71c793694a3a74c4acd451399e3
ta4ilka69 <artem_balin_2004@mail.ru>
2023-06-14 22:39:52 +0300

    закрытие опд

---
+++ ОПД/7/dop.asm
@@ -0,0 +1,19 @@
+ma E0
+mw 0020009001
+mw 0010009110
+mw 81E6041040
+mw 0010809110
+mw 0001009021
+mw 80E2101040
+mw 0010E09001
+mw 80C4101040
+
+org 0x10
+first_num: word 0xC000
+res: word ?
+start: cla
+    ld #2
+    word 0x9010
+    st $res
+    hlt
+end
